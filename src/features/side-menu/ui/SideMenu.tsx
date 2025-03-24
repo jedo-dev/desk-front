914dee6ce00d558d655a6a45432efd330c14a7fd
Sanya Domashnyi ^_^ <megamanok99@gmail.com>
2025-03-25 00:48:13 +0300

    test

--- src/features/side-menu/ui/SideMenu.tsx
+++ src/features/side-menu/ui/SideMenu.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const data = [
   {
-    title: 'Ant Design Title 1',
+    title: 'Ant Design Title 1', 
   },
   {
-    title: 'Ant Design Title 2',
+    title: 'Ant Design Title 222',
   },
   {
     title: 'Ant Design Title 3',
